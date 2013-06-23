b57b1ac3b3f10d48e6e6570b44d4cc8c6d054b99
Github User <this@isafakeadress.lose.tld>
2013-06-23 12:18:32 +0100

    Start
    ok we have a dictionary with everything in it now need to count cycles

---
+++ ProjectEuler74/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler74
+{
+    class Program
+    {
+
+        static int DigitFactorialSum(int x)
+        {
+            int answer = 0;
+
+            int[] facts = { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880 };
+
+            while (x > 0)
+            {
+                answer += facts[x % 10];
+                x /= 10;
+            }
+
+            return answer;
+        }
+
+        static void Main(string[] args)
+        {
+            const int max = 2177281; // so the dict has everything beneath 1MM
+            SortedDictionary<int, int> Facts = new SortedDictionary<int, int>();
+
+            for (int i = 1; i <= max; i++)
+            {
+                Facts.Add(i, DigitFactorialSum(i));
+            }
+            Console.WriteLine(Facts.Max(x => x.Value));
+
+        }
+    }
+}
